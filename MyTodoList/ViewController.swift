c58d9cca6b861a04137f2d754e94fe62236fc36e
Taichi Matsuura <ta-matsuura@kddi-tech.com>
2017-06-15 07:41:10 +0900

    first commit

--- MyTodoList/ViewController.swift
+++ MyTodoList/ViewController.swift
@@ -8,8 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource, UITableViewDelegate {
 
+    var todolist: [String] = [String]()
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func tapActionButton(_ sender: Any) {
+        
+        let alertController = UIAlertController(title: "TODO追加", message: "TODOを入力してください", preferredStyle: UIAlertControllerStyle.alert)
+        
+        //テキストエリア追加
+        alertController.addTextField(configurationHandler: nil)
+        
+        //OKボタン追加
+        if let textField = alertController.textFields?.first {
+            //先頭に入力値を挿入する
+            self.todolist.insert(textField.text!, at: 0)
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
 }
 
